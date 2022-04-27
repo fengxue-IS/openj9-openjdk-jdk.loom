516374abb4e7e92e1d74df7b95367bbdd269f9b7
Jack Lu <Jack.S.Lu@ibm.com>
2022-04-27 10:35:44 -0400

    fix threadStatus reference
    Signed-off-by: Jack Lu <Jack.S.Lu@ibm.com>

--- src/java.base/share/classes/java/lang/Thread.java
+++ src/java.base/share/classes/java/lang/Thread.java
@@ -3113,7 +3113,7 @@ public class Thread implements Runnable {
 
     private void setPriority0(int newPriority) {
         synchronized (interruptLock) {
-            if ((threadStatus != 0) && (NO_REF != eetop)) {
+            if ((holder.threadStatus != 0) && (NO_REF != eetop)) {
                 setPriorityNoVMAccessImpl(eetop, newPriority);
             }
         }
@@ -3122,7 +3122,7 @@ public class Thread implements Runnable {
     private void stop0(Object o) {
         synchronized (interruptLock) {
             if (o != null) {
-                if (threadStatus == 0) {
+                if (holder.threadStatus == 0) {
                     /* [PR CMVC 179978] Java7:JCK:java_lang.Thread fails in all platform */
                     /*
                         * If the thread has not yet been simply store the fact that stop has been called,
@@ -3165,7 +3165,7 @@ public class Thread implements Runnable {
 
     private void setNativeName(String name) {
         synchronized (interruptLock) {
-            if ((threadStatus != 0) && (eetop != NO_REF)) {
+            if ((holder.threadStatus != 0) && (eetop != NO_REF)) {
                 setNameImpl(eetop, name);
             }
         }
@@ -3376,7 +3376,7 @@ public class Thread implements Runnable {
     private boolean isDead() {
         // Has already started, is not alive anymore, and has been removed from the ThreadGroup
         synchronized (interruptLock) {
-            return ((threadStatus != 0) && (eetop == NO_REF));
+            return ((holder.threadStatus != 0) && (eetop == NO_REF));
         }
     }
 
